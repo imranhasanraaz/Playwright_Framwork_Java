cb5b586f70ede71c977eb57ecd2073668f97f0b9
Imran Hasan <imranhasanraaz@gmail.com>
2023-12-20 20:33:02 +0600

    Clean Code

--- src/test/java/test_runners/TestRunner.java
+++ src/test/java/test_runners/TestRunner.java
@@ -10,7 +10,7 @@ import org.testng.annotations.DataProvider;
         plugin = {
                 "pretty",
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-                "timeline:test-output-thread/"
+                "rerun:target/rerun.txt"
         }
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
