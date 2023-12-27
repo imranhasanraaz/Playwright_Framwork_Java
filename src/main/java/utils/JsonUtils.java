7ec61a4411a244cc615f2960fddb4127b9af9374
Imran Hasan <imranhasanraaz@gmail.com>
2023-12-27 14:50:22 +0600

    parallel support

--- src/main/java/utils/JsonUtils.java
+++ src/main/java/utils/JsonUtils.java
@@ -1,4 +1,4 @@
-package utilities;
+package utils;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
--- src/main/java/utils/ResourceFile.java
+++ src/main/java/utils/ResourceFile.java
@@ -1,4 +1,4 @@
-package utilities;
+package utils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
